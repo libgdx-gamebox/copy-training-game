ae01c6a43307a66eed5d1365d9a8ab38e4687d74
ronjay1 <ronjay@laivdata.com>
2025-05-15 22:26:38 +0900

    feat: handling spriteBatch size rendering using public resources

--- core/src/main/java/com/ronjay/game/Main.java
+++ core/src/main/java/com/ronjay/game/Main.java
@@ -3,6 +3,7 @@ package com.ronjay.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.GL20;
@@ -13,15 +14,18 @@ import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 public class Main extends ApplicationAdapter {
     private SpriteBatch spriteBatch;
     private Texture image;
-    private Texture sandTexture;
 
+    public Sprite textureSprite;
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
         image = new Texture("libgdx.png");
 
-        sandTexture = new Texture(Gdx.files.internal("Dawnlike.png"));
+        sandTexture = new Texture(Gdx.files.internal("Tileable_Dirt_Textures/Dirt_01_Nrm.png"));
+        textureSprite = new Sprite(sandTexture);
+        textureSprite.setBounds(0, 0, 200,200);
+
     }
 
     @Override
@@ -32,13 +36,20 @@ public class Main extends ApplicationAdapter {
 //        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         spriteBatch.begin();
 //        spriteBatch.draw(image, 140, 210);
-        spriteBatch.draw(sandTexture, 0, 0);
+        textureSprite.draw(spriteBatch);
+
         spriteBatch.end();
     }
 
     @Override
     public void dispose() {
         spriteBatch.dispose();
+        spriteBatch.dispose();
         image.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height); // update projection matrix
     }
 }
